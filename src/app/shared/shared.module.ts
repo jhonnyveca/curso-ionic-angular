03dea5b0eeb144f4b5eaf71426a9bf5de8483215
jhonnyveca <jhonnyvecadev@gmail.com>
2023-06-04 12:04:33 -0500

    curso ionic angular

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  exports: [RouterModule],
+  imports: [CommonModule, RouterModule],
+  providers: [],
+})
+export class SharedModule {}
